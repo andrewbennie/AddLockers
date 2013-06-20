6e47bdbd09ddd35411d6080893cb18601b573e19
Andrew Bennie <andrew.bennie@gmail.com>
2013-06-20 15:31:08 -0400

    Initial Commit

---
+++ AddLockers/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AddLockers
+//
+//  Created by Andrew Bennie on 6/20/13.
+//  Copyright (c) 2013 self.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ AddLockers/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AddLockers
+//
+//  Created by Andrew Bennie on 6/20/13.
+//  Copyright (c) 2013 self.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
